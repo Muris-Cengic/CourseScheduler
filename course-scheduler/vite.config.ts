43e8eb64cd2e1570dcda2acc340901402f7e3446
Muris.Cengic <muris.cengic@outlook.com>
2025-08-26 18:17:23 +0400

    Adjust repo path in vite.config

--- course-scheduler/vite.config.ts
+++ course-scheduler/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: "/https://github.com/Muris-Cengic/CourseScheduler.git/",
+  base: "/CourseScheduler/",
   plugins: [react()],
   resolve: { alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) } },
   css: { transformer: "postcss", postcss: "./postcss.config.js" },
